b721e7671b0e8c0aa0839d7f092cbb2a2c2946dc
Henry Widd <henryw374@gmail.com>
2025-04-04 12:04:21 +0100

    add pages for tutorial

---
+++ src/dev.clj
@@ -0,0 +1,31 @@
+(ns dev
+  (:require [clojure.java.io :as io]
+            [com.widdindustries.tiado-cljs2 :as util]
+            [babashka.fs :as fs]))
+
+
+(defn app-config
+  ""
+  []
+  (->
+    (util/browser-app-config {:watch-dir  "resources/public"
+                              :asset-path "/js"})
+    (merge
+      {:modules          {:main {:entries ['app.core]}}})))
+
+(defn app-release []
+  (util/prod-build
+    (-> (app-config)
+        (assoc :compiler-options {;:pretty-print true
+                                  ;:pseudo-names true
+                                  })
+        (dissoc :devtools))))
+
+(comment
+  
+  (util/watch (app-config))
+
+  (app-release )
+  (util/build-report (app-config) "build-report.html")
+
+  )
